4958cd9eaa778e7257bc5d743f6904130db7eba5
wangxiaoke <wangxiaoke@cestc.com>
2024-12-03 14:40:47 +0800

    m

--- ch1_hello.cpp
+++ ch1_hello.cpp
@@ -1,9 +1,7 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+ 
 #include <iostream>
-#define STB_IMAGE_IMPLEMENTATION
-#include "stb_image.h"
-
  
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
